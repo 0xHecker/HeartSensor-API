8067cb38887b189324a997f85637ab12e49c0906
xHecker <91829843+xHecker@users.noreply.github.com>
2022-03-18 15:31:50 +0530

    removed html

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { get } = require("lodash");
 const mongoose = require("mongoose");
 const query = require("./routes/query");
 const app = express();
@@ -10,22 +9,11 @@ const uri =
   "mongodb://shanmukh:shanmukh123@heartdata1-shard-00-00.p4bql.mongodb.net:27017,heartdata1-shard-00-01.p4bql.mongodb.net:27017,heartdata1-shard-00-02.p4bql.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-a8729b-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(uri)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-// createNewData();
-
 app.use("/api/query", query);
-
-app.post("/hi", async (req, res) => {
-  const data = await Data.find();
-  res.send("Hello there");
-  console.log(data);
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
